37544209564df12fb532cdece3a42434f87d392d
Chris Deck <deckchris95@gmail.com>
2025-02-21 17:21:42 -0500

    analytics in diff spot

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,8 +97,8 @@ export default async function RootLayout({
             </div>
           )} */}
           {children}
+          <Analytics />
         </main>
-        <Analytics />
         <footer className="bg-muted">
           <div className="container mx-auto px-4 py-8">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
